27aca5cdc4a29ba6322d2228c9650f3c37ce20cd
Elise St Hilaire and Matt Caswell <elise+matt@>
2017-03-15 11:30:50 -0700

    Add api link

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -5,5 +5,11 @@ $(document).ready(function() {
     var city = $('#city').val();
     var distance = $('#distance').val();
     var year = $('#year').val();
+    $('#color').val("");
+    $('#make').val("");
+    $('#city').val("");
+    $('#distance').val("");
+    $('#year').val("");
+    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + make + ' &location=' + city + '&distance=' + distance + '&stolenness=stolen&year=' + year + '&access_token=ab24acd3ed8bffe04230ed7b746b35a0d8ad027b2dcc150fac3341def8eee73e')
   });
 });
